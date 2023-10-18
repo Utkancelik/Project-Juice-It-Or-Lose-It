dddc2e81b19a3b9033c84c974d39feec165beb4c
Utkan Çelik <utkancelik@hacettepe.edu.tr>
2023-10-18 15:51:16 +0300

    I fixed issues with collisions and added boundaries and mobile input system for movement. Upcoming weeks will add features like power-ups, different bricks and paddle model.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -8,11 +8,38 @@ public class BallMovement : MonoBehaviour
 
     private Rigidbody2D rb;
 
-    
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         // Set the initial velocity of the ball
         rb.velocity = initialDirection.normalized * initialSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            // Adjust ball's velocity based on where it hits the paddle.
+            float hitOffset = (transform.position.x - collision.transform.position.x) / collision.collider.bounds.size.x;
+            Vector2 newDirection = new Vector2(hitOffset, 1).normalized;
+
+            // Apply the new direction to the ball with the same speed.
+            rb.velocity = newDirection * initialSpeed;
+        }
+        else if (collision.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("Collided with: " + collision.gameObject.tag);
+            Debug.Log("Collision normal: " + collision.contacts[0].normal);
+
+            rb.velocity = rb.velocity.normalized * initialSpeed;
+        }
+        else if (collision.gameObject.CompareTag("Brick"))
+        {
+            // Handle brick collision here, e.g., destroy the brick and reflect the ball.
+            Destroy(collision.gameObject);
+
+            // Normalize the direction after hitting the brick to maintain a consistent speed.
+            rb.velocity = rb.velocity.normalized * initialSpeed;
+        }
     }
 }
--- Assets/Scripts/PaddleMovement.cs
+++ Assets/Scripts/PaddleMovement.cs
@@ -2,29 +2,67 @@ using UnityEngine;
 
 public class PaddleMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Adjust the speed as needed
+    public float moveSpeed = 10f;     // Adjust the speed as needed.
+    public float mobileMoveSpeed = 5f; // Mobile-specific speed.
+    public bool mobileInputEnabled = true; // Enable or disable mobile input.
 
     private float paddleWidth;
     private float screenBoundsX;
 
+    private Vector3 touchStartPos;
+    private Vector3 touchEndPos;
+    private bool isDragging = false;
+
     private void Start()
     {
-        paddleWidth = transform.localScale.x; // Get the paddle's width
+        paddleWidth = transform.localScale.x; // Get the paddle's width.
         screenBoundsX = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
     }
 
     private void Update()
     {
-        // Get the horizontal input (left or right arrow key)
-        float horizontalInput = Input.GetAxis("Horizontal");
+        // Handle paddle movement using keyboard or touch input.
+        if (mobileInputEnabled && Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
 
-        // Calculate the new position of the paddle
-        Vector3 newPosition = transform.position + Vector3.right * horizontalInput * moveSpeed * Time.deltaTime;
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    touchStartPos = touch.position;
+                    isDragging = true;
+                    break;
 
-        // Limit the paddle's movement within the screen boundaries
-        newPosition.x = Mathf.Clamp(newPosition.x, -screenBoundsX + paddleWidth / 2, screenBoundsX - paddleWidth / 2);
+                case TouchPhase.Moved:
+                    if (isDragging)
+                    {
+                        touchEndPos = touch.position;
+                        float delta = touchEndPos.x - touchStartPos.x;
+                        Vector3 newPosition = transform.position + Vector3.right * (delta / Screen.width) * mobileMoveSpeed * Time.deltaTime;
+                        newPosition.x = Mathf.Clamp(newPosition.x, -screenBoundsX + paddleWidth / 2, screenBoundsX - paddleWidth / 2);
+                        transform.position = newPosition;
+                        touchStartPos = touch.position;
+                    }
+                    break;
 
-        // Apply the new position to the paddle
-        transform.position = newPosition;
+                case TouchPhase.Ended:
+                    isDragging = false;
+                    break;
+            }
+        }
+        else
+        {
+            // Get the horizontal input (left or right arrow key)
+            float horizontalInput = Input.GetAxis("Horizontal");
+
+            // Calculate the new position of the paddle
+            Vector3 newPosition = transform.position + Vector3.right * horizontalInput * moveSpeed * Time.deltaTime;
+
+            // Limit the paddle's movement within the screen boundaries
+            newPosition.x = Mathf.Clamp(newPosition.x, -screenBoundsX + paddleWidth / 2 + .5f, screenBoundsX - paddleWidth / 2 - .5f);
+
+            // Apply the new position to the paddle
+            transform.position = newPosition;
+        }
     }
 }
